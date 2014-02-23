51d9f81cf38e91246fa34519c2181e5437e824ac
Daniel Klein <daniel.klein.dmk@gmail.com>
2014-02-22 20:28:35 -0600

    Started implementing the algorithm itself, piece by piece, writing tests as I go along... fun times.

--- despy.py
+++ despy.py
@@ -5,7 +5,10 @@ Daniel Klein
 So, on a whim I've decided to take a shot at implementing the DES encryption 
 algorithm in highly readable Python, without a ton of design forethought.
 Here goes.
+
+Reference: http://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm
 '''
+from tables import *
 
 
 class DESpy():
@@ -18,7 +21,7 @@ class DESpy():
 		Return the exclusive-OR'ed result of two binary strings (1's and 0's).
 		'''
 		if len(bitsA) != len(bitsB):
-			return None
+			raise Exception("Strings must be same length in order to XOR")
 
 		newBits = ["1"]*len(bitsA)
 		for i in range(len(bitsA)):
@@ -44,22 +47,55 @@ class DESpy():
 	# end left_shift()
 
 
+	def permute_key_with_pc1(self, key):
+		if len(key) != 64:
+			raise Exception("Key length must be 64 bits.")
+		for char in key:
+			if char != '0' and char != '1':
+				raise Exception("Key must contain only 0's and 1's.")
+		
+		permuted_key = [None]*56
+		for i in range(len(PC1_TABLE)):
+			permuted_key[i] = key[PC1_TABLE[i] - 1]
+		return ''.join(permuted_key)
+	# end permute_key_with_pc1()
+
+
+	def create_shifted_blocks(self, init_block):
+		blocks = []
+		cur_block = init_block
+		for num_shifts in SHIFT_TABLE:
+			next_block = self.left_shift(cur_block, num_shifts)
+			blocks.append(next_block)
+			cur_block = next_block
+		return blocks
+
+
 	def encrypt(self, plain_text, key):
 		'''
-		step 1: create 16 subkeys, each 48 bits long
-			(i) permute key using PC-1 table (to 56 bit key)
-			(ii) split into left and right halves (C0 and D0, 28 bits each)
-			(iii) from C0 and D0, create 16 blocks from each using left shifts table
-			(iv) from each CnDn form key Kn by using PC-2 table (48 bits)
-		step 2: encode each 64-bit block of data
-			(i) apply initial permutation by using IP table on 64-bit data
-			(ii) divide IP block into L0 and R0 halves, 32 bits each
-			(iii) iterate 16 times on:
+		TODO: step 1: create 16 subkeys, each 48 bits long
+			(i) permute key using PC-1 table (to 56 bit key) -- DONE
+			(ii) split into left and right halves (C0 and D0, 28 bits each) -- DONE
+			TODO: (iii) from C0 and D0, create 16 blocks from each using left shifts table
+			TODO: (iv) from each CnDn form key Kn by using PC-2 table (48 bits)
+		TODO: step 2: encode each 64-bit block of data
+			TODO: (i) apply initial permutation by using IP table on 64-bit data
+			TODO: (ii) divide IP block into L0 and R0 halves, 32 bits each
+			TODO: (iii) iterate 16 times on:
 				Ln = Rn-1
 				Rn = Ln-1 XOR f(Rn-1,Kn)
 				using f function
-			(iv) apply final permutation IP^-1
+			TODO: (iv) apply final permutation IP^-1
 		'''
+		# (i) permute key using PC-1 table (to 56 bit key)
+		permuted_key = permute_key_with_pc1(key)
+		if len(permuted_key) != 56:
+			raise Exception("Length of permuted key should be 56 bits.")
+
+		# (ii) split into left and right halves (C0 and D0, 28 bits each)
+		c_zero = permuted_key[0:28]
+		d_zero = permuted_key[28:]
+
 		pass
 	# end encrypt()
 
--- test_despy.py
+++ test_despy.py
@@ -25,6 +25,7 @@ class TestDespy(unittest.TestCase):
 
 
 	def test_XOR(self):
+		print "**Testing XOR function...**"
 		expected = "11111111"
 		actual = self.despy.XOR(self.test1, self.test2)
 		print "{0} XOR {1} is: {2}".format(self.test1, self.test2, actual) 
@@ -40,13 +41,12 @@ class TestDespy(unittest.TestCase):
 		print "{0} XOR {1} is: {2}".format(self.test3, self.test4, actual) 
 		self.assertEqual(expected, actual)
 
-		expected = None
-		actual = self.despy.XOR(self.test5, self.test6)
-		print "{0} XOR {1} is: {2}".format(self.test5, self.test6, actual) 
-		self.assertEqual(expected, actual)
+		self.assertRaises(Exception, self.despy.XOR, self.test5, self.test6)
+		print "Finished testing XOR function."
 
 
 	def test_left_shift(self):
+		print "**Testing left-shift function...**"
 		expected = "00011110"
 		actual = self.despy.left_shift(self.test1,1)
 		print "{0} left shifted 1 is: {1}".format(self.test1, actual)
@@ -86,6 +86,74 @@ class TestDespy(unittest.TestCase):
 		actual = self.despy.left_shift(self.test1,8)
 		print "{0} left shifted 8 is: {1}".format(self.test1, actual)
 		self.assertEqual(expected, actual)
+		print "**Finished testing left-shift function.**"
+
+
+	def test_permute_key_with_pc1(self):
+		print "**Testing PC1 key permutation...**"
+		self.assertRaises(Exception, self.despy.permute_key_with_pc1, "01010101")
+		self.assertRaises(Exception, self.despy.permute_key_with_pc1, "01010101010101010101010101010101010101010101010101010101ABCDEFGH")
+ 
+		test_key = "0001001100110100010101110111100110011011101111001101111111110001"
+		expected = "11110000110011001010101011110101010101100110011110001111"
+		actual = self.despy.permute_key_with_pc1(test_key)
+		print "Key before permutation: {0}".format(test_key)
+		print "Expected permuted key:  {0}".format(expected)
+		print "Actual permuted key:    {0}".format(actual)
+		self.assertEqual(expected, actual) 
+		print "**Finished testing PC1 key permutation.**"
+
+
+	def test_create_shifted_blocks(self):
+		print "**Testing creation of shifted key blocks...**"
+		c_zero = "1111000011001100101010101111"
+		d_zero = "0101010101100110011110001111"
+		c_blocks = ([
+					"1110000110011001010101011111",
+					"1100001100110010101010111111",
+					"0000110011001010101011111111",
+					"0011001100101010101111111100",
+					"1100110010101010111111110000",
+					"0011001010101011111111000011",
+					"1100101010101111111100001100",
+					"0010101010111111110000110011",
+					"0101010101111111100001100110",
+					"0101010111111110000110011001",
+					"0101011111111000011001100101",
+					"0101111111100001100110010101",
+					"0111111110000110011001010101",
+					"1111111000011001100101010101",
+					"1111100001100110010101010111",
+					"1111000011001100101010101111"
+					])
+		d_blocks = ([
+					"1010101011001100111100011110",
+					"0101010110011001111000111101",
+					"0101011001100111100011110101",
+					"0101100110011110001111010101",
+					"0110011001111000111101010101",
+					"1001100111100011110101010101",
+					"0110011110001111010101010110",
+					"1001111000111101010101011001",
+					"0011110001111010101010110011",
+					"1111000111101010101011001100",
+					"1100011110101010101100110011",
+					"0001111010101010110011001111",
+					"0111101010101011001100111100",
+					"1110101010101100110011110001",
+					"1010101010110011001111000111",
+					"0101010101100110011110001111"
+					])
+		expected = c_blocks
+		actual = self.despy.create_shifted_blocks(c_zero)
+		self.assertEqual(expected, actual)
+		print "Created C blocks match expected."
+
+		expected = d_blocks
+		actual = self.despy.create_shifted_blocks(d_zero)
+		self.assertEqual(expected, actual)
+		print "Created D blocks match expected."
+		print "**Finished testing creation of shifted key blocks...**"
 
 
 if __name__ == '__main__':
